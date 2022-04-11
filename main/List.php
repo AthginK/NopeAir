9c80e535471c717b7aba244d04d2c85149f292cd
peak4442 <peak4442@gmail.com>
2022-04-11 11:05:00 +0700

    pass&php

---
+++ main/List.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+  
+<head>
+    <title>Insert to database</title>
+</head>
+  
+<body>    
+        <?php  
+
+        include 'connect.php';    
+          
+        // Taking all 8 values from the form data(input)
+
+        $title =  $_POST['title'];
+        $firstname =  $_POST['firstname'];
+        $lastname = $_POST['lastname'];
+        $email = $_POST['email'];
+        $tel = $_POST['tel'];
+        $date_of_birth = $_POST['date_of_birth'];
+                
+        // SQL script for performing insert query execution
+        $sql = "INSERT INTO passenger (title, firstname, lastname, email, tel, date_of_birth)
+        VALUES ('$title', '$firstname', '$lastname', '$email', '$tel', '$date_of_birth')";
+
+
+        if(mysqli_query($conn, $sql)){
+            echo "<h3>The user data is stored in a database successfully.</h3>";   
+        } else{
+            echo "ERROR: Hush! Sorry $sql. " 
+                . mysqli_error($conn);
+        }
+          
+        // Close connection
+        mysqli_close($conn);
+        ?>
+
+</body>
+  
+</html>
