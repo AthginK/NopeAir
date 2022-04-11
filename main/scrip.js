e18cc102332cb3f671b4ffec7769159304e6d643
Kwanlada1244 <63070213@kmitl.ac.th>
2022-04-11 12:59:48 +0700

    pim

--- main/scrip.js
+++ main/scrip.js
@@ -1,3 +1,4 @@
+// <------------------------Home-------------------->
 function Oneway_display() {
     document.getElementById('roundtrip_select').style.display = 'none';
     document.getElementById('oneway_select').style.display = 'block';
@@ -8,4 +9,45 @@ function Oneway_display() {
 function RoundTrip_display() {
     document.getElementById('oneway_select').style.display = 'none';
     document.getElementById('roundtrip_select').style.display = 'block';
+}
+
+// <------------------------Extra service------------------------->
+function SelectSeat() {
+    var btn = document.getElementById('SeatBtn').innerHTML;
+    if (btn=="Close"){
+        document.getElementById('SeatBtn').innerHTML = "Select";
+    }
+    else {
+        document.getElementById('SeatBtn').innerHTML = "Close"
+    }
+}
+
+function SelectBaggage() {
+    var btn = document.getElementById('BaggageBtn').innerHTML;
+    if (btn=="Close"){
+        document.getElementById('BaggageBtn').innerHTML = "Edit";
+    }
+    else {
+        document.getElementById('BaggageBtn').innerHTML = "Close"
+    }
+}
+
+function SelectSpecial() {
+    var btn = document.getElementById('SpecialBtn').innerHTML;
+    if (btn=="Close"){
+        document.getElementById('SpecialBtn').innerHTML = "Add";
+    }
+    else {
+        document.getElementById('SpecialBtn').innerHTML = "Close"
+    }
+}
+
+function SelectFirst() {
+    var btn = document.getElementById('FirstBtn').innerHTML;
+    if (btn=="Close"){
+        document.getElementById('FirstBtn').innerHTML = "Add";
+    }
+    else {
+        document.getElementById('FirstBtn').innerHTML = "Close"
+    }
 }
