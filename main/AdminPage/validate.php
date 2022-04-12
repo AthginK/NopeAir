d9e1ed71089afdb94bab32826a57b0ab7bb884f8
63070233 <63070233@it.kmitl.ac.th>
2022-04-12 13:50:26 +0700

    n

---
+++ main/AdminPage/validate.php
@@ -0,0 +1,32 @@
+<?php
+ 
+ include 'connect.php';
+   
+$username = $_POST["username"];
+$password = $_POST["password"];
+
+if ($_SERVER["REQUEST_METHOD"]== "POST") {
+
+    // SQL script for selecting by username
+    $sql = "SELECT * FROM admin WHERE username='$username'"; 
+    
+    if($result = mysqli_query($conn, $sql)){       
+        foreach($result as $user) {       
+            
+            if(($user['username'] == $username) && 
+                ($user['password'] == $password)) {
+                    header("Location: http://localhost/main/NopeAir/main/AdminPage/index.html");
+            }
+            else {
+                echo "<script language='javascript'>";
+                echo "alert('WRONG INFORMATION ')";
+                echo "</script>";
+                die();
+            }
+        } 
+    } else{
+        echo "ERROR: Hush! Sorry $sql. " 
+            . mysqli_error($conn);
+    }
+
+}
