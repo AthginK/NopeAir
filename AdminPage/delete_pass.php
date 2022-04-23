66c67d13fc50c5f9f8935107f70f65693f22cb68
peak4442 <peak4442@gmail.com>
2022-04-23 15:50:40 +0700

    Fixed delete_pass

--- AdminPage/delete_pass.php
+++ AdminPage/delete_pass.php
@@ -4,9 +4,11 @@
     
     // SQL script for deleting an item
     $sql = "DELETE FROM passenger WHERE pass_id = $id";
-    $sql = "DELETE FROM booking WHERE pass_id = $id";
-    $sql = "DELETE FROM payment WHERE pass_id = $id";
+    $sql1 = "DELETE FROM booking WHERE pass_id = $id";
+    $sql2 = "DELETE FROM payment WHERE pass_id = $id";
     
-    mysqli_query($conn,$sql);    
+    mysqli_query($conn,$sql);
+    mysqli_query($conn,$sql1);
+    mysqli_query($conn,$sql2);   
     header("location:passengers.php");
 ?>
